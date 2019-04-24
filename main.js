90dca712b4c59ebafe5cab13d5cf5aa31de038f7
lgrinter <lgrinter@live.com>
2019-04-24 01:27:31 +0000

    Final Version

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 let Phrase = require("lgrinter-palindrome");
 
-let string = prompt("Please enter a string for palindome testing:");
-let phrase = new Phrase(string);
+function palindromeTester(event) {
+    event.preventDefault();
+    
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
-} else {
-    alert(`"${phrase.content}" is not a palindrome.`)
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML =`"${phrase.content}" is a palindrome!`;
+    }
+    else {
+        palindromeResult.innerHTML =`"${phrase.content}" is not a palindrome.`;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+        palindromeTester(event);
+    });
+});
